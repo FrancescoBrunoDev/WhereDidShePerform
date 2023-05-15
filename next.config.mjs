fbe1545d8c6fef2325b4abddb82d0003ca7072bd
franzbruno95 <59353843+franzbruno95@users.noreply.github.com>
2023-05-15 10:59:31 +0200

    cors

--- next.config.mjs
+++ next.config.mjs
@@ -5,26 +5,6 @@ const nextConfig = {
     appDir: true,
     esmExternals: "loose",
   },
-  async headers() {
-    return [
-      {
-        source: "/app/api/musiconn.js",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-          },
-        ],
-      },
-    ]
-  },
 }
 
 export default nextConfig
