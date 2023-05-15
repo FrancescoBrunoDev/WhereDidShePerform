c6be46fbe112f607fd0123d011554d65826e1be3
franzbruno95 <59353843+franzbruno95@users.noreply.github.com>
2023-05-15 10:54:48 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -8,7 +8,7 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: "/app/api/:path*",
+        source: "/app/api/musiconn.js",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
