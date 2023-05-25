9ba4cb59a484af37246d3564dc0623c7c87479c9
franzbruno95 <59353843+franzbruno95@users.noreply.github.com>
2023-05-25 14:25:27 +0200

    yml test

--- components/suggestions/suggestions.jsx
+++ components/suggestions/suggestions.jsx
@@ -4,7 +4,7 @@ import { TopPersons } from "./topPersons"
 
 export default function Suggestions() {
   return (
-    <div className="flex snap-x scroll-pl-6 flex-nowrap gap-4 overflow-x-scroll pt-1">
+    <div className="flex snap-x scroll-pl-6 flex-nowrap gap-4 overflow-x-scroll pt-1 pr-10">
       <TopPersons />
       <TopPerfomers />
       <RandomCard />
