c9906a2bb10c6393e7902130f20f9d2801a51ba2
franzbruno95 <59353843+franzbruno95@users.noreply.github.com>
2023-06-02 22:36:57 +0200

    linkMaker improved

--- utils/linkMaker.js
+++ utils/linkMaker.js
@@ -1,66 +1,60 @@
 export function linkMaker(title) {
   const itemMatch = title.match(
     /(\d{1,2})\.(\d{1,2})\.(\d{4})(?:, (\d{1,2}):(\d{2}))?$/
-  );
+  )
+  const personMatch = title.match(/(.+?)\s*\(([^)]+)\)/)
+
   if (itemMatch) {
-    const itemString = `${itemMatch[3]}-${itemMatch[2].padStart(
+    const [, day, month, year, hours, minutes] = itemMatch
+    const itemString = `-${year}-${month.padStart(2, "0")}-${day.padStart(
       2,
       "0"
-    )}-${itemMatch[1].padStart(2, "0")}`;
-    const hasTime = itemMatch[4] && itemMatch[5];
-
-    let titleWithoutItem = title.replace(
+    )}`
+    const timeString =
+      hours && minutes ? `-${hours.padStart(2, "0")}-${minutes}` : ""
+    const titleWithoutItem = title.replace(
       /,\s*\d{1,2}\.\d{1,2}\.\d{4}(?:, \d{1,2}:\d{2})?$/,
       ""
-    );
-    let titleLink = `${titleWithoutItem.replace(/\s+/g, "-")}`;
-    if (itemString) {
-      titleLink += `-${itemString}`;
-    }
-    if (hasTime) {
-      const timeString = `${itemMatch[4].padStart(2, "0")}-${itemMatch[5]}`;
-      titleLink += `-${timeString}`;
-    }
-
-    const cleanedTitleLink = titleLink
+    )
+    const cleanedTitleLink = `${titleWithoutItem
       .toLowerCase()
-      .replace(/[,()\[\].\/]/g, "")
+      .replace(/[^\w\säöüß-]/g, "")
       .replace(/ä/g, "ae")
       .replace(/ö/g, "oe")
       .replace(/ü/g, "ue")
       .replace(/ß/g, "ss")
-      .replace(/-{2,}/g, "-")
-      .replace(/,$/, "")
-      .replace(/--/g, "-");
-
-    return cleanedTitleLink;
+      .replace(/\s+/g, "-")
+      .replace(/-{2,}/g, "-")}${itemString}${timeString}`
+    return cleanedTitleLink
   }
 
-  const personMatch = title.match(/(.+?),\s*(.+)/);
   if (personMatch) {
-    const lastName = personMatch[1]
+    const [, lastName, firstName] = personMatch
+    const cleanedTitleLink = `${lastName
       .toLowerCase()
-      .replace(/,/g, "")
-      .replace(/\s+/g, "-");
-    const firstName = personMatch[2]
+      .replace(/[^\w\säöüß-]/g, "")
+      .replace(/ä/g, "ae")
+      .replace(/ö/g, "oe")
+      .replace(/ü/g, "ue")
+      .replace(/ß/g, "ss")
+      .replace(/\s+/g, "-")}-${firstName
       .toLowerCase()
-      .replace(/,/g, "")
-      .replace(/\s+/g, "-");
-    const cleanedTitleLink = `${lastName}-${firstName}`;
-
-    return cleanedTitleLink;
+      .replace(/[^\w\säöüß-]/g, "")
+      .replace(/ä/g, "ae")
+      .replace(/ö/g, "oe")
+      .replace(/ü/g, "ue")
+      .replace(/ß/g, "ss")
+      .replace(/\s+/g, "-")}`
+    return cleanedTitleLink
   }
 
   const cleanedTitleLink = title
     .toLowerCase()
-    .replace(/[,()\[\].\/]/g, "")
+    .replace(/[^\w\säöüß-]/g, "")
     .replace(/ä/g, "ae")
     .replace(/ö/g, "oe")
     .replace(/ü/g, "ue")
     .replace(/ß/g, "ss")
-    .replace(/-{2,}/g, "-")
-    .replace(/,/g, "")
-    .replace(/\s+/g, "-");
-
-  return cleanedTitleLink;
+    .replace(/\s+/g, "-")
+  return cleanedTitleLink
 }
