eb8fce39e139bc2faf6b49aa64f6745aa4fd87a6
franzbruno95 <59353843+franzbruno95@users.noreply.github.com>
2023-06-02 22:14:15 +0200

    Update site.ts

--- config/site.ts
+++ config/site.ts
@@ -3,7 +3,7 @@ export type SiteConfig = typeof siteConfig
 export const siteConfig = {
   name: "Where did s.he performed?",
   description:
-    "Explore the extensive catalogue of Musiconn Performance with an interactive map.",
+    "Explore the extensive catalogue of Musiconn Performance with interactive maps.",
   mainNav: [
 /*     {
       title: "Home",
