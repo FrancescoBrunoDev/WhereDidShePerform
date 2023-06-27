0540f3ca06b4c4d0669552be6b30cbf3cc8d5f1a
franzbruno95 <59353843+FrancescoBrunoDev@users.noreply.github.com>
2023-06-27 09:14:59 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,22 +2,12 @@ module.exports = {
   reactStrictMode: true,
   experimental: {
     appDir: true,
-    esmExternals: 'loose'
+    esmExternals: "loose",
   },
   images: {
-    domains: ['uploadthing.com', 'lh3.googleusercontent.com'],
+    domains: ["uploadthing.com", "lh3.googleusercontent.com"],
   },
-  async headers() {
-    return [
-      {
-        source: "/app/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "https://musiconnmap.vercel.app" },
-          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-        ],
-      },
-    ];
+  eslint: {
+    ignoreDuringBuilds: true,
   },
-};
+}
