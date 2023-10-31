2c9670493855bb1e749af6a3d4b90760c653da4b
franzbruno95 <59353843+FrancescoBrunoDev@users.noreply.github.com>
2023-10-31 13:55:01 +0100

    metadata fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import "@/styles/globals.css"
-import { Metadata } from "next"
+import { Metadata, Viewport } from "next"
 
 import { siteConfig } from "@/config/site"
 import { fontSans } from "@/lib/fonts"
@@ -7,8 +7,14 @@ import { cn } from "@/lib/utils"
 import { Toaster } from "@/components/ui/toaster"
 import Providers from "@/components/Providers"
 import { SiteHeader } from "@/components/site-header"
-import { TailwindIndicator } from "@/components/tailwind-indicator"
 import { ThemeProvider } from "@/components/theme-provider"
+
+export const viewport: Viewport = {
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "white" },
+    { media: "(prefers-color-scheme: dark)", color: "black" },
+  ],
+}
 
 export const metadata: Metadata = {
   title: {
@@ -16,17 +22,12 @@ export const metadata: Metadata = {
     template: `%s - ${siteConfig.name}`,
   },
   description: siteConfig.description,
-  themeColor: [
-    { media: "(prefers-color-scheme: light)", color: "white" },
-    { media: "(prefers-color-scheme: dark)", color: "black" },
-  ],
   icons: {
     icon: "/favicon.ico",
     shortcut: "/favicon-16x16.png",
     apple: "/apple-touch-icon.png",
   },
 }
-
 
 export default function RootLayout({
   children,
@@ -52,7 +53,6 @@ export default function RootLayout({
               {children}
               {authModal}
 
-              {/* <TailwindIndicator /> */}
             </ThemeProvider>
             <Toaster />
           </Providers>
